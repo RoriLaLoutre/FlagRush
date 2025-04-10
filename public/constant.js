40d9516a24892c64a7212cc67b66c00e36f99b7b
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-04-10 14:42:15 +0200

    add texture plus quelque murs

--- public/constant.js
+++ public/constant.js
@@ -7,6 +7,6 @@ export const server = "http://flag.techtwins.fr/";
 export const speed = 8; // vitesse de déplacement des joueurs
 export const taille_map = 15; // taille de la map
 
-export const pesanteur = -250; // force de gravité
+export const pesanteur = -7.5; // force de gravité
 export const hauteurMur = 7.5; 
 
