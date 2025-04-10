8200062a3181e7e4523a0551f28790655ab09c8d
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-04-10 16:59:04 +0200

    ajout des spawn et direction camera par defaut plus changement zone et couleur

--- public/lightings/light.js
+++ public/lightings/light.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.1/build/three.module.js";  
-
+import {taille_map} from "../constant.js";
 
 export const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(5, 10, 5);
@@ -11,10 +11,10 @@ light.shadow.mapSize.width = 1024;
 light.shadow.mapSize.height = 1024;
 light.shadow.camera.near = 0.5;
 light.shadow.camera.far = 50;
-light.shadow.camera.left = -10;
-light.shadow.camera.right = 10;
-light.shadow.camera.top = 10;
-light.shadow.camera.bottom = -10;
+light.shadow.camera.left = -taille_map;
+light.shadow.camera.right = taille_map;
+light.shadow.camera.top = taille_map;
+light.shadow.camera.bottom = -taille_map;
 
 // Ambient light (soft fill light)
 export const ambient = new THREE.AmbientLight(0xffffff, 0.3);
